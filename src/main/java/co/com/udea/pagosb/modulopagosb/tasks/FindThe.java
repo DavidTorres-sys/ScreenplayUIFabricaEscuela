118ad97d6eda60688a4d593b3acafc7ec5f574c4
DavidTorres-sys <david.torresg@udea.edu.co>
2024-11-19 15:03:27 -0500

    xpath for some target

--- src/main/java/co/com/udea/pagosb/modulopagosb/tasks/FindThe.java
+++ src/main/java/co/com/udea/pagosb/modulopagosb/tasks/FindThe.java
@@ -5,8 +5,11 @@ import net.serenitybdd.screenplay.Actor;
 
 public class FindThe implements Task {
 
+    private String target;
 
     @Override
-    public <T extends Actor> void performAs(T actor) {}
+    public <T extends Actor> void performAs(T actor) {
+
+    }
 
 }
--- src/main/java/co/com/udea/pagosb/modulopagosb/userinterfaces/UserPage.java
+++ src/main/java/co/com/udea/pagosb/modulopagosb/userinterfaces/UserPage.java
@@ -3,7 +3,15 @@ package co.com.udea.pagosb.modulopagosb.userinterfaces;
 import net.serenitybdd.screenplay.targets.Target;
 
 public class UserPage {
-    public static final Target FLIGHT_FARE_DROPDOWN = Target.the("Flight fare dropdown").locatedBy("");
-    public static final Target TAXES_DROPDOWN = Target.the("Taxes dropdown").locatedBy("");
-    public static final Target ADDITIONAL_CHARGES_DROPDOWN = Target.the("Additional charges dropdown").locatedBy("");
+    public static final Target FLIGHT_FARE_DROPDOWN = Target.the("Flight fare dropdown")
+            .locatedBy("//*[@id=\"id-price-title\"]");
+
+    public static final Target TAXES_DROPDOWN = Target.the("Taxes dropdown")
+            .locatedBy("//*[@id=\"id-price-title\"]");
+
+    public static final Target ADDITIONAL_CHARGES_DROPDOWN = Target.the("Additional charges dropdown")
+            .locatedBy("//*[@id=\"id-price-title\"]");
+
+    public static final Target PURCHASE_SUMMARY_PAGE_TITLE = Target.the("Detalles de tu vuelo")
+            .locatedBy("//*[@id=\"__next\"]/main/div/div[1]/div/div[1]/div[1]/div/h2");
 }
